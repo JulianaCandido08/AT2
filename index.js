ce406ceeb827cc328a5b991860d01e1027df0197
unknown <you@example.com>
2024-09-19 01:56:59 -0300

    segundo commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const sqlite3 = require('sqlite3').verbose();
+
+const app = express();
+
+const port = 3000;
+
+const db = new sqlite3.Database(':memory:');
+
+db.serialize(() => {
+    db.run("CREATE TABLE user (id INT, name TEXT)");
+
+    const stmt = db.prepare("INSERT INTO user (id, name) VALUES (?, ?)");
+    stmt.run(1, 'Juliana Candido');
+    stmt.run(2, 'ETE GIL');
+    stmt.finalize();
+});
+
+app.get('/users', (req, res) => {
+    db.all("SELECT * FROM user", [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+app.listen(port, () => {
+    console.log('Servidor rodando em http://localhost:${port}');
+});
